abf4cbb853aab0611ac865486816cbfe82beefc4
ANA LUIZA APARECIDA DA SILVA <23111775@CFP127ALUNO.LOCAL>
2023-11-10 12:21:27 -0300

    terminado o chat

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+
+$(".input_text").focus(function(){
+    $(this).prev('.fa').addclass('glowIcon')
+})
+$(".input_text").focusout(function(){
+    $(this).prev('.fa').removeclass('glowIcon')
+})
